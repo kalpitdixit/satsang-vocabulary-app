c53b94cdd0ee10a5949354511b8b872c9d6e6ab8
Kalpit Dixit <kddixit@amazon.com>
2024-12-02 16:12:40 -0800

    updates

---
+++ main_app/commands.sh
@@ -0,0 +1,28 @@
+# build
+flet build web
+
+# moves build files out of repo
+rm -rf ../../b
+mv build ../../b
+
+# commit branch main
+ga -u
+gc -m "updates"
+
+# checkout branch gh-pages-2
+git checkout gh-pages-2
+
+# copy build files
+cd ..
+rm -rf *
+cp -R ../b/web/* .
+
+# remove line from index.html
+head -n 14 index.html > tmp.html
+tail -n+16 index.html >> tmp.html
+mv -f tmp.html index.html
+
+# commit and push gh-pages-2
+ga -u
+gc -m "updates"
+git push
