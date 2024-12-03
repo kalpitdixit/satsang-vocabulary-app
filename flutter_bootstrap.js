6b0f176b4ba552b1eb70f11ac094a9af1532bbcb
Kalpit Dixit <kddixit@amazon.com>
2024-12-02 18:48:42 -0800

    updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2891530475"
+    serviceWorkerVersion: "3013132828"
   }
 });
