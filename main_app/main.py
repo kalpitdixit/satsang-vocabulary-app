ba4b778f0522c8178ccf70c4ef766de38032fdfc
Kalpit Dixit <kddixit@amazon.com>
2024-12-06 18:36:39 -0800

    updates

--- main_app/main.py
+++ main_app/main.py
@@ -313,6 +313,7 @@ class Orchestrator:
                            "height" : page.height}
             
         # OAuth
+        """
         self.provider = Auth0OAuthProvider(
             client_id="ioQ4UgajAgoAxz2PahBbeuSrs8zrLEu8",
             client_secret="11M8ZGiC727PS75w8BBBOyKYx96-3UAMOYUqTj0dLnH5N77AYqO9NQOU_lEQVknV",
@@ -320,6 +321,7 @@ class Orchestrator:
             #redirect_url="http://localhost:8550/api/oauth/redirect"
             redirect_url="http://localhost:8550/oauth_callback",
         )
+        """
         self.page.on_login = self.on_login
 
         # Memory and Personalization
